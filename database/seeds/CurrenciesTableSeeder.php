86db37800c603d99c49e5584c2d1e14a70013aa7
Jhersy <jhersyvb@gmail.com>
2020-06-30 13:02:48 -0500

    7. Preparando la plataforma de pagos y llenando la base de datos desde Laravel

---
+++ database/seeds/CurrenciesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CurrenciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currencies = [
+            'usd',
+            'eur',
+            'gbp',
+        ];
+
+        foreach ($currencies as $currency) {
+            \App\Currency::create([
+                'iso' => $currency,
+            ]);
+        }
+    }
+}
